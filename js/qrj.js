c9c96e85f29747021ae74af37f32ef8580ac949c
ai3895674 <1364506743@qq.com>
2018-06-03 12:04:16 +0800

    6.3早上修改

---
+++ js/qrj.js
@@ -0,0 +1,58 @@
+
+$(function(){
+
+    $('.box>label').click(function(){
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    $(".box2>div").click(function(){
+
+        $(this).addClass('on2').siblings().removeClass('on2');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
